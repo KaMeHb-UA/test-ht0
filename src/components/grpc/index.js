2e8f5d8e60b2c7a455b58b931aa20133bae08b28
KaMeHb-UA <marlock@bassteam.ml>
2022-06-17 05:02:28 +0300

    add gRPC component & proto

---
+++ src/components/grpc/index.js
@@ -0,0 +1,35 @@
+import { resolve } from 'node:path';
+import { promisify } from 'node:util';
+import { load } from '@grpc/proto-loader';
+import { loadPackageDefinition, Server, ServerCredentials } from '@grpc/grpc-js';
+
+export const name = 'gRPC Handler';
+
+function wrapMethods(obj){
+    const res = {};
+    for(const name in obj){
+        res[name] = async ({ request }, callback) => {
+            try{
+                callback(null, await obj[name](request));
+            } catch(e){
+                callback(e);
+            }
+        };
+    }
+    return res;
+}
+
+export default async ({ appRoot, loadComponent, grpcBinding }) => {
+    const [ methods, packageDefinition ] = await Promise.all([
+        loadComponent('Methods').then(wrapMethods),
+        load(resolve(appRoot, 'grpc.proto')),
+    ]);
+    const proto = loadPackageDefinition(packageDefinition);
+    return async () => {
+        const server = new Server();
+        server.addService(proto.LogService.service, methods);
+        await promisify(server.bindAsync).call(server, grpcBinding, ServerCredentials.createInsecure());
+        server.start();
+        return promisify(server.tryShutdown).bind(server);
+    };
+};
