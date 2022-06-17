fb69d4e1c458d7153be4a52dc474a6ac9b5d8c65
KaMeHb-UA <marlock@bassteam.ml>
2022-06-17 16:02:14 +0300

    destroy methods handle

--- src/components/server/index.js
+++ src/components/server/index.js
@@ -6,11 +6,16 @@ const handlerComponents = [
 export const name = 'Server';
 
 export default async ({ loadComponent }) => {
+    const methodsDestroy = loadComponent('Methods').then(({ destroy }) => destroy);
     const handlers = await Promise.all(handlerComponents.map(loadComponent));
     return async () => {
         const stoppers = await Promise.all(handlers.map(start => start()));
+        stoppers.push(await methodsDestroy);
         return async () => {
+            console.log('\n\nShutdown the server...');
+            const start = Date.now();
             await Promise.all(stoppers.map(stop => stop()));
+            console.log(`${name} closed in ${Date.now() - start} ms`);
         };
     };
 };
