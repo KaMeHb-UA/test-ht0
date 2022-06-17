c3a1bd9128e43dad1fff66620bd62377258bf42b
KaMeHb-UA <marlock@bassteam.ml>
2022-06-17 05:18:14 +0300

    log startup time

--- src/services/component-registration/index.js
+++ src/services/component-registration/index.js
@@ -2,9 +2,10 @@ const loadedComponents = {};
 
 export function registerComponent({ name, default: loader }, options){
     if(name in loadedComponents) throw new Error('Cannot register component ' + name + ' twice');
+    let start;
     loadedComponents[name] = new Promise(r => setTimeout(r))
-        .then(() => loader(options))
-        .then(component => loadedComponents[name] = component);
+        .then(() => (start = Date.now(), loader(options)))
+        .then(component => (console.log(`Component ${name} loaded in ${Date.now() - start} ms`), loadedComponents[name] = component));
 }
 
 export async function loadComponent(name){
