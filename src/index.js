1c163bf121e2c6a00102e6d53fd148b7cf21e80c
KaMeHb-UA <marlock@bassteam.ml>
2022-06-17 05:09:27 +0300

    app launcher

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import loadComponent from '@/components';
+import process from 'node:process';
+
+void async function(){
+    const startServer = await loadComponent('Server');
+    const stopServer = await startServer();
+    process.on('SIGINT', stopServer);
+    process.on('SIGHUP', stopServer);
+}();
